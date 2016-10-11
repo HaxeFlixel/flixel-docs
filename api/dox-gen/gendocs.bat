780f9ec6870e0c9df8d30b7e52224877c4658e77
Gama11 <jensfischer95@gmail.com>
2016-10-11 19:03:08 +0200

    gendocs version bump

--- api/dox-gen/gendocs.bat
+++ api/dox-gen/gendocs.bat
@@ -1,1 +1,1 @@
-haxelib run dox -i xml/bin/flash/types.xml -o .. -theme ./theme --title "HaxeFlixel API" -D version 4.1.1 --exclude "(__ASSET__|ApplicationMain|DocumentClass|DefaultAssetLibrary|Main|NMEPreloader|zpp_nape)"
+haxelib run dox -i xml/bin/flash/types.xml -o .. -theme ./theme --title "HaxeFlixel API" -D version 4.2.0 --exclude "(__ASSET__|ApplicationMain|DocumentClass|DefaultAssetLibrary|Main|NMEPreloader|zpp_nape)"
